531ee82b719bd92d6a3d54cacebacf79b7234702
S M <sm-soft@live.com>
2020-10-30 13:41:43 +0300

    Add project files.

---
+++ LogServer/Class.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogServer
+{
+    public class Class : DbContext
+    {
+        public Class(DbContextOptions<Class> options) : base(options)
+        {
+
+        }
+        public DbSet<Data> Datas { get; set; }
+    }
+
+    public class Data
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ LogServer/Migrations/20201028141151_init.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LogServer.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Datas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datas", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Datas");
+        }
+    }
+}
